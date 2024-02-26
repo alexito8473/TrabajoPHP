bdb61c1283b0e9819e1303b1a06989257c8892c4
aleja <aagualb451@g.educaand.es>
2024-02-26 15:44:47 +0100

    Mejora el sistema

--- prinicpal.php
+++ prinicpal.php
@@ -9,31 +9,83 @@
 </head>
 
 <body>
+    <header>
+        <div>
+            <h1>Panel de control</h1>
+        </div>
+    </header>
     <div class="separador">
         <div class="container">
-            <header>
-                <h1>Panel de control</h1>
-            </header>
             <main>
                 <section class="seccion">
+                    <div><img src="Utilidad/educacion.png"></div>
                     <h2>Oferta educativa</h2>
-                    <p>Texto de la sección 1.</p>
+                    <p>Una oferta educativa es el conjunto de programas, cursos y oportunidades de aprendizaje que una
+                        institución educativa ofrece a sus estudiantes. Va más allá de simples materias, abarcando desde
+                        educación formal hasta formación profesional y educación continua. </p>
+                    <div class="form1">
+                        <form action="logicaOferta.php" method="get"><input class="saltar" type="submit"
+                                name="verOferta" value="Ver tabla de la oferta educativa"></form>
+                    </div>
+                    <div class="form2">
+                        <form action="logicaOferta.php" method="get"><input class="saltar" type="submit"
+                                name="añadirOferta" value="Añadir nueva oferta educativa"></form>
+                    </div>
                 </section>
                 <section class="seccion">
+                    <div><img src="Utilidad/curso.png"></div>
                     <h2>Curso</h2>
-                    <p>Texto de la sección 2.</p>
+                    <p>Un curso es una unidad específica dentro de la oferta educativa, diseñada para enseñar un tema
+                        particular en un período determinado. Puede ser parte de un programa más amplio o impartirse de
+                        manera independiente. Los cursos están estructurados para proporcionar un enfoque coherente y
+                        sistemático de aprendizaje sobre un tema específico</p>
+                    <div class="form1">
+                        <form action="logicaOferta.php" method="get"><input class="saltar" type="submit"
+                                name="verOferta" value="Ver tabla de la oferta educativa"></form>
+                    </div>
+                    <div class="form2">
+                        <form action="logicaOferta.php" method="get"><input class="saltar" type="submit"
+                                name="añadirOferta" value="Añadir nueva oferta educativa"></form>
+                    </div>
                 </section>
                 <section class="seccion">
+                    <div><img src="Utilidad/profesor.png"></div>
                     <h2>Profesor</h2>
-                    <p>Texto de la sección 2.</p>
+                    <p>Un profesor es un guía, un facilitador y un modelo a seguir en el proceso educativo. Más allá de transmitir conocimientos, un buen profesor inspira, motiva y desafía a sus estudiantes a alcanzar su máximo potencial.</p>
+                    <div class="form1">
+                        <form action="logicaOferta.php" method="get"><input class="saltar" type="submit"
+                                name="verOferta" value="Ver tabla de la oferta educativa"></form>
+                    </div>
+                    <div class="form2">
+                        <form action="logicaOferta.php" method="get"><input class="saltar" type="submit"
+                                name="añadirOferta" value="Añadir nueva oferta educativa"></form>
+                    </div>
                 </section>
                 <section class="seccion">
+                    <div><img src="Utilidad/asignatura.png"></div>
                     <h2>Asignatura</h2>
-                    <p>Texto de la sección 2.</p>
+                    <p>Una asignatura es una unidad específica de estudio dentro de un plan de estudios, diseñada para abordar un área temática particular. Cada asignatura tiene objetivos de aprendizaje definidos y está estructurada para desarrollar competencias y habilidades específicas en los estudiantes. </p>
+                    <div class="form1">
+                        <form action="logicaOferta.php" method="get"><input class="saltar" type="submit"
+                                name="verOferta" value="Ver tabla de la oferta educativa"></form>
+                    </div>
+                    <div class="form2">
+                        <form action="logicaOferta.php" method="get"><input class="saltar" type="submit"
+                                name="añadirOferta" value="Añadir nueva oferta educativa"></form>
+                    </div>
                 </section>
                 <section class="seccion">
+                    <div><img src="Utilidad/horario.png"></div>
                     <h2>Horario</h2>
-                    <p>Texto de la sección 2.</p>
+                    <p>Los horarios educativos son como mapas que guían el día a día de estudiantes y profesores en su jornada de aprendizaje. Estos horarios asignan bloques de tiempo para actividades académicas, desde clases y conferencias hasta periodos de estudio independiente y descansos.</p>
+                    <div class="form1">
+                        <form action="logicaOferta.php" method="get"><input class="saltar" type="submit"
+                                name="verOferta" value="Ver tabla de la oferta educativa"></form>
+                    </div>
+                    <div class="form2">
+                        <form action="logicaOferta.php" method="get"><input class="saltar" type="submit"
+                                name="añadirOferta" value="Añadir nueva oferta educativa"></form>
+                    </div>
                 </section>
             </main>
             <footer>
